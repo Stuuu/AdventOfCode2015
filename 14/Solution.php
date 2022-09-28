5c4bf8a352f9ea10fd5daa17483399cdc21b93a0
Stuuu <stuartschrick@gmail.com>
2022-09-28 09:01:34 -0400

    day 14 part 2

--- 14/Solution.php
+++ 14/Solution.php
@@ -14,6 +14,17 @@
 
 // Given the descriptions of each reindeer (in your puzzle input), after exactly 2503 seconds, what distance has the winning reindeer traveled?
 
+// --- Part Two ---
+// Seeing how reindeer move in bursts, Santa decides he's not pleased with the old scoring system.
+
+// Instead, at the end of each second, he awards one point to the reindeer currently in the lead. (If there are multiple reindeer tied for the lead, they each get one point.) He keeps the traditional 2503 second time limit, of course, as doing otherwise would be entirely ridiculous.
+
+// Given the example reindeer from above, after the first second, Dancer is in the lead and gets one point. He stays in the lead until several seconds into Comet's second burst: after the 140th second, Comet pulls into the lead and gets his first point. Of course, since Dancer had been in the lead for the 139 seconds before that, he has accumulated 139 points by the 140th second.
+
+// After the 1000th second, Dancer has accumulated 689 points, while poor Comet, our old champion, only has 312. So, with the new scoring system, Dancer would win (if the race ended at 1000 seconds).
+
+// Again given the descriptions of each reindeer (in your puzzle input), after exactly 2503 seconds, how many points does the winning reindeer have?
+
 
 class Solution
 {
@@ -47,6 +58,14 @@ class Solution
                     }
                 }
             }
+
+            $top_distance_this_second =  max(array_column($reindeer_stats, 'distance_traveled'));
+
+            foreach ($reindeer_stats as $key => &$value) {
+                if($value['distance_traveled'] === $top_distance_this_second ){
+                    $value['wins']++;
+                }
+            }
         }
 
         print_r($reindeer_stats);
@@ -65,6 +84,7 @@ class Solution
                 'rest_time' => $input_parts[13],
                 'rest_until_second' => null,
                 'distance_traveled' => 0,
+                'wins' => 0,
             ];
         }
         return $stats;
