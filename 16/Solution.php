c1f35a5a7b83abb3c49a03e913f530536fb27e08
Stuuu <stuartschrick@gmail.com>
2022-10-07 05:34:33 -0400

    day 16 part 2

--- 16/Solution.php
+++ 16/Solution.php
@@ -31,6 +31,13 @@
 
 // What is the number of the Sue that got you the gift?
 
+// --- Part Two ---
+// As you're about to send the thank you note, something in the MFCSAM's instructions catches your eye. Apparently, it has an outdated retroencabulator, and so the output from the machine isn't exact values - some of them indicate ranges.
+
+// In particular, the cats and trees readings indicates that there are greater than that many (due to the unpredictable nuclear decay of cat dander and tree pollen), while the pomeranians and goldfish readings indicate that there are fewer than that many (due to the modial interaction of magnetoreluctance).
+
+// What is the number of the real Aunt Sue?
+
 
 class Solution
 
@@ -60,11 +67,29 @@ class Solution
         foreach ($aunts as $aunt_num => $aunt) {
             $debug[$aunt_num]['items'] = $aunt;
             foreach ($aunt as $item => $val) {
-                $debug[$aunt_num]['matches'][$item] = ($match_vals[$item] === $val) ? 1 : 0;
+
+                switch ($item) {
+                    case 'cats':
+                        $debug[$aunt_num]['matches'][$item] = ($match_vals[$item] < $val) ? 1 : 0;
+                        break;
+                    case 'trees':
+                        $debug[$aunt_num]['matches'][$item] = ($match_vals[$item] < $val) ? 1 : 0;
+                        break;
+                    case 'pomeranians':
+                        $debug[$aunt_num]['matches'][$item] = ($match_vals[$item] > $val) ? 1 : 0;
+                        break;
+                    case 'goldfish':
+                        $debug[$aunt_num]['matches'][$item] = ($match_vals[$item] > $val) ? 1 : 0;
+                        break;
+
+                    default:
+                        $debug[$aunt_num]['matches'][$item] = ($match_vals[$item] === $val) ? 1 : 0;
+                        break;
+                }
             }
             $debug[$aunt_num]['match_count'] = array_sum($debug[$aunt_num]['matches']);
 
-            if($debug[$aunt_num]['match_count'] >= 3){
+            if ($debug[$aunt_num]['match_count'] >= 3) {
                 echo $aunt_num . PHP_EOL;
             }
         }
@@ -85,8 +110,6 @@ class Solution
                 $split_parts = explode(": ", $part);
                 $aunts[$sue_num + 1][$split_parts[0]] = (int) $split_parts[1];
             }
-
-
         }
         return $aunts;
     }
